b0a8b848ce624df641cadf4711a24ec11b4409cc
eko-prasetio <caedablerg@gmail.com>
2024-08-26 00:10:57 +0700

    update available room checkout

--- routes/web.php
+++ routes/web.php
@@ -88,4 +88,5 @@ Route::controller(FrontendRoomController::class)->group(function () {
     Route::get('/room/details/{id}', 'RoomDetailsPage')->name('detail.room');
     Route::get('/bookings/', 'BookingSeach')->name('booking.search');
     Route::get('/search/room/details/{id}', 'SearchRoomDetails')->name('search_room_details');
+    Route::get('/check_room_availability/', 'CheckRoomAvailability')->name('check_room_availability');
 });
